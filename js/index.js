84685fc4bcd95e8e400de10c64d83d41634d64c8
belloemmanuel <belloemmanuel02@gmail.com>
2023-05-16 14:54:09 +0100

    async

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// const goToWork =()=>{
+//     function takeBreakFast(){
+        
+//     }
+//     function wearYourCloth(){
+
+//     }
+//     function getInsideTheCar(){
+
+//     }
+// } synchronous function
+
+
+const goToWork = async()=>{
+  function takeBreakFast(){
+
+  } 
+  function wearYourCloth(){
+
+  }
+  function getInsideTheCar(){
+
+  }
+  await takeBreakFast()
+  await wearYourCloth()
+  await getInsideTheCar()
+}
+
+goToWork();
+
+
+//asynchronous function
+
+
