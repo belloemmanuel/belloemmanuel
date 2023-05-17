5b773c1263f72342053118a2ca7abfe3c0454949
belloemmanuel <belloemmanuel02@gmail.com>
2023-05-17 19:21:13 +0100

    style made to about page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-
-
-
-
-
+document.addEventListener('mousemove', function(e) {
+    let body = document.querySelector('body');
+    let image = document.getElementById('#rocketpicture');
+    let left = e.offsetX;
+    let top = e.offsetY;
+    image.style.left = left + 'px';
+    image.style.top = top + 'px';
+  });
