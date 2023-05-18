dcb4534ffd0301f7e0a5a77f69617201be4d9c14
belloemmanuel <belloemmanuel02@gmail.com>
2023-05-18 06:29:41 +0100

    menu

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-document.addEventListener('mousemove', function(e) {
-    let body = document.querySelector('body');
-    let image = document.getElementById('#rocketpicture');
-    let left = e.offsetX;
-    let top = e.offsetY;
-    image.style.left = left + 'px';
-    image.style.top = top + 'px';
-  });
+let closeNav = document.getElementById('closenav');
+let mobileNav = document.getElementById('mobile-nav')
+let menuEl = document.getElementById('menu')
+
+let close = () =>{
+  mobileNav.style.display = 'none'
+}
+
+// let open = () =>{
+//   mobileNav.style.display = 'block'
+//   console.log('menu')
+// }
+
+menuEl.addEventListener('click', open)
+// closeNav.addEventListener('click', close)
+
+
+
