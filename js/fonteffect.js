080352de2b227014ae1b4dbbb01a89e13ca0018e
belloemmanuel <belloemmanuel02@gmail.com>
2023-05-21 19:58:50 +0100

    mobile nav updated

--- js/fonteffect.js
+++ js/fonteffect.js
@@ -9,14 +9,3 @@ glowInTexts.forEach(glowInText => {
     glowInText.append(span);
   });
 });
-
-const bg = document.querySelector('.background-image');
-const windowWidth = window.innerWidth / 5;
-const windowHeight = window.innerHeight / 5 ;
-
-bg.addEventListener('mousemove', (e) => {
-  const mouseX = e.clientX / windowWidth;
-  const mouseY = e.clientY / windowHeight;
-  
-  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-});
